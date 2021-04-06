42b300ac924efe087b885a7bac2c9799c88cd069
joshitanvi <tanvijoshi04@gmail.com>
2021-04-06 18:01:35 +0530

    ball movement

--- sketch.js
+++ sketch.js
@@ -1,24 +1,35 @@
 var ball;
 
+var mydb, mypos;
+
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    mydb = firebase.database();
+   // console.log(db);
+
+    mypos = mydb.ref('ball/position');
+    console.log(mypos);
+
+    mypos.on('value', readPos);
+
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePos(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePos(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePos(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePos(0,+1);
     }
     drawSprites();
 }
@@ -26,4 +37,19 @@ function draw(){
 function changePosition(x,y){
     ball.x = ball.x + x;
     ball.y = ball.y + y;
+}
+
+function readPos(data){
+    console.log(data.val())
+    ball.x = data.val().x;
+    ball.y = data.val().y;
+
+}
+function writePos(x, y){
+
+    mypos.set({
+        'x': ball.x + x,
+        'y': ball.y + y 
+    })
+
 }
